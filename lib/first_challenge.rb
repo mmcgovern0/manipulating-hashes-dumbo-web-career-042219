5c72a35f0353dc0c1d39b6d09b8cda346f94d679
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-14 20:45:28 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -18,6 +18,8 @@ def first_challenge
     if attribute == :favorite_icecream_flavors
       value.delete_if do |flavor|
         flavor == "strawberry"
+      end
+    end
       
 
   #remember to return your newly altered contacts hash!
